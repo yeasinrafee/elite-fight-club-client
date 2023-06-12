7c5b9b3a7ae27c8c4265edfead105c35e7177189
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-06-12 11:48:19 +0600

    Navbar and footer

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,57 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+  const navMenu = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link>Instructors</Link>
+      </li>
+      <li>
+        <Link>Classes</Link>
+      </li>
+    </>
+  );
   return (
     <div>
-      <h1>Navbar</h1>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navMenu}
+            </ul>
+          </div>
+          <Link to="/" className="btn btn-ghost normal-case h-16 ">
+            <h2 className="text-2xl font-bold">
+              EliteFight <br />C L U B
+            </h2>
+          </Link>
+        </div>
+        <div className="navbar-end hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navMenu}</ul>
+        </div>
+      </div>
     </div>
   );
 };

f6929f9723e7741d0ca6e2b2b260949e3bb6dcb0
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-06-12 12:05:22 +0600

    nav footer color

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
       <li>
         <Link>Classes</Link>
       </li>
+      {/* TODO: Dynamic Options */}
     </>
   );
   return (
@@ -37,19 +38,21 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-amber-100 rounded-box w-52"
             >
               {navMenu}
             </ul>
           </div>
           <Link to="/" className="btn btn-ghost normal-case h-16 ">
-            <h2 className="text-2xl font-bold">
+            <h2 className="text-2xl font-bold text-amber-400">
               EliteFight <br />C L U B
             </h2>
           </Link>
         </div>
         <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{navMenu}</ul>
+          <ul className="menu menu-horizontal px-1 text-amber-400 md:text-lg">
+            {navMenu}
+          </ul>
         </div>
       </div>
     </div>

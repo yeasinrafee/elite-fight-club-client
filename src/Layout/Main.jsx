ba20e74984876a149450e1bdc316163fdf5748bb
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-06-12 11:03:56 +0600

    First commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/router.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="max-w-screen-2xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+);

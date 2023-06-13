399754bf5abcf170cad25752ef5c044e69e68914
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-06-13 22:16:58 +0600

    Instructor page added

--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -8,6 +8,29 @@ const Instructors = () => {
       <h2 className="text-4xl font-bold text-amber-400 text-center uppercase my-12 ">
         Introducing Our Honorable Instructors
       </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 justify-content-center items-center gap-9 my-5 md:my-24">
+        {instructors &&
+          instructors.map((instructor) => (
+            <div
+              key={instructor._id}
+              className="card w-80 h-96 mx-auto bg-base-100 shadow-xl"
+            >
+              <figure className="px-10 pt-10">
+                <img
+                  src={instructor.image}
+                  alt="instructor image"
+                  className="rounded-xl"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title text-amber-400">
+                  Name: {instructor.name}
+                </h2>
+                <p>Email: {instructor.email}</p>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };

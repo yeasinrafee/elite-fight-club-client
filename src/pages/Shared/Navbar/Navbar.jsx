8236a80f1a19bfd152dc92a352c412105531bec5
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-06-13 17:47:57 +0600

    navbar bug fixed

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
               <Tippy content={user && user?.displayName}>
                 {user?.photoURL ? (
                   <img
-                    className="w-[65px] h-[60px] rounded-full"
+                    className="w-[65px] h-[50px] rounded-full"
                     src={user.photoURL}
                     alt="User Image"
                   />

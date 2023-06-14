b4c25d4820d2867b062108d534992b43a9d1d6ee
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-06-14 15:02:41 +0600

    Using AxiosSecure api

--- src/hooks/useClasses.jsx
+++ src/hooks/useClasses.jsx
@@ -1,15 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useClasses = () => {
+  const [axiosSecure] = useAxiosSecure();
   const {
     isLoading,
     refetch,
     data: classes = [],
   } = useQuery({
     queryKey: ["classes"],
+    // queryFn: async () => {
+    //   const response = await fetch(`http://localhost:5000/classes`);
+    //   return response.json();
+    // },
     queryFn: async () => {
-      const response = await fetch(`http://localhost:5000/classes`);
-      return response.json();
+      const response = await axiosSecure(`/classes`);
+
+      return response.data;
     },
   });
 
